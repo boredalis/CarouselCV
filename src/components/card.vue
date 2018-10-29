c74fbd19b64b288feab1016a10ee7e8be0358399
Aurora Selimi <selimi.aurora@gmail.com>
2018-10-29 15:20:54 +0100

    personal page

---
+++ src/components/card.vue
@@ -0,0 +1,152 @@
+<template>
+  <div id="theCard" ref="theCard" @click.stop="toggleRowActive" :class="{'active': active}">
+    <div class="left"> 
+      <p style="text-align:center;width:150px;max-height:200px;overflow:scroll; margin-top:40px;font-weight:100;">
+      <a href="">{{note.smName}}</a> {{note.more}} </p></div>
+    <div class="center">
+      <h2 style="font-family: 'Quicksand', sans-serif;color:black;">{{note.smName}}</h2>
+      <p>{{note.description}}</p>
+      <div style="display: flex; align-items: center; justify-content: center;    height: calc(100% - 200px);"><img class="imazhet" :src="note.url" alt=""></div>
+    </div>
+    <div class="theIcons">
+        <div style="display:flex;">
+          <svg class="feather feather-twitter sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#dda77d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" data-reactid="1206"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
+          <svg class="feather feather-package sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#dda77d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M12.89 1.45l8 4A2 2 0 0 1 22 7.24v9.53a2 2 0 0 1-1.11 1.79l-8 4a2 2 0 0 1-1.79 0l-8-4a2 2 0 0 1-1.1-1.8V7.24a2 2 0 0 1 1.11-1.79l8-4a2 2 0 0 1 1.78 0z"></path><polyline points="2.32 6.16 12 11 21.68 6.16"></polyline><line x1="12" y1="22.76" x2="12" y2="11"></line><line x1="7" y1="3.5" x2="17" y2="8.5"></line></svg>
+          <svg class="feather feather-github sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#dda77d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+        </div>
+        <div style="display:flex;">
+          <svg class="feather feather-gitlab sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="#dda77d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M22.65 14.39L12 22.13 1.35 14.39a.84.84 0 0 1-.3-.94l1.22-3.78 2.44-7.51A.42.42 0 0 1 4.82 2a.43.43 0 0 1 .58 0 .42.42 0 0 1 .11.18l2.44 7.49h8.1l2.44-7.51A.42.42 0 0 1 18.6 2a.43.43 0 0 1 .58 0 .42.42 0 0 1 .11.18l2.44 7.51L23 13.45a.84.84 0 0 1-.35.94z"></path></svg>
+          <svg class="feather feather-feather sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="#dda77d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M20.24 12.24a6 6 0 0 0-8.49-8.49L5 10.5V19h8.5z"></path><line x1="16" y1="8" x2="2" y2="22"></line><line x1="17" y1="15" x2="9" y2="15"></line></svg>
+        </div>
+    </div>
+    <button>
+    <svg class="feather feather-search sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="20" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" data-reactid="1001"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: "card",
+  props:['note','active','index'],
+  methods:{
+    toggleRowActive() {
+      this.$emit('newactive', this.index);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+  #theCard{
+    position:relative;
+    margin:15px;
+    opacity: 1;
+    box-shadow: 0 5px 20px 0px rgba(82, 82, 82, 0.5);
+    background-color:white;
+    cursor:pointer;  
+    opacity:0.5;
+    padding:0 10px;
+    border-radius:10px;
+    height:380px;
+  }
+  #theCard.active{
+    border-radius:0;
+    align-items:center;
+    justify-content:center;
+    padding-left:0;
+    padding-right:0;
+    margin: 15px 220px 30px 220px;
+    opacity:1;
+    .left{
+      animation:slide-left .5s cubic-bezier(.25,.46,.45,.94)  both;
+      opacity:1;
+    }
+    .theIcons{
+      animation:slide-right .5s cubic-bezier(.25,.46,.45,.94) both;
+      opacity:1;
+    }
+    button{
+      display:flex;
+      &:hover{
+        animation: myfirst .2s linear forwards;
+      }
+    }
+  }
+  .left{
+    position: absolute;
+    height: 100%;
+    display: flex;
+    border-radius: 10px 0 0 10px;
+    align-items: center;
+    background: white;
+    left:0;
+    opacity:0;
+    height:100%;
+    z-index:0;
+    justify-content: center;
+    width: 200px;
+    white-space:normal;
+    p{
+      a{
+      color:#8e8989;
+      //margin-right:2px;
+      &:hover{
+        color:#565656;
+
+      }
+
+    }
+    }
+    ::-webkit-scrollbar { 
+     display: none; 
+    }
+  }
+  .center{
+    background:white;
+    align-items:center;
+    height:100%;
+    z-index: 10;
+    width: 300px;
+  }
+  h2{
+    font-weight:500;
+    margin: 0;
+    padding-top: 20px;
+  }
+  .imazhet{
+    -webkit-user-select: none; 
+    max-width: 70%;
+  }
+  .theIcons{
+    background: white;
+    position: absolute;
+    height: 100%;
+    top: 0;
+    width: 200px;
+    border-radius: 0px 10px 10px 0px;
+    right:0;
+    opacity:0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    svg{
+      padding:5px;
+    }
+  }
+  button{
+    position:absolute;
+    border:none;
+    background-color:#d6a37b;
+    border-radius:100%;
+    color:white;
+    padding:10px;
+    outline:none;
+    left: calc(50% - 19px);
+    top: calc(100% - 18px);
+    display:none;
+    cursor:pointer;
+  }
+  @keyframes slide-left{0%{transform:translateX(0)}100%{transform:translateX(-200px)}}
+  @keyframes slide-right{0%{transform:translateX(0)}100%{transform:translateX(200px)}}
+  @keyframes myfirst {0%{top:calc(100% - 18px)}100%{top:calc(100% - 24px)}}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,154 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img class="bg" style="-webkit-user-select: none;"src="https://images.unsplash.com/photo-1449247709967-d4461a6a6103?ixlib=rb-0.3.5&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;s=1053a3cdbabdd85102b762b3a05966ae&amp;auto=format&amp;fit=crop&amp;w=1502&amp;q=80">
+        <div class="container" ref="container">
+            <div class="carousel-item-spacer">
+                <p>This is a spacer</p>
+            </div>
+            <div style="display:flex;">
+                <card v-for="(note,index) in notes" :class="noteClass(index)" :active="index === activeIndex" :index="index" :note="note" @newactive="hapuSusam(index)"></card>
+            </div>
+            <div class="carousel-item-spacer">
+                <p>This is a spacer</p>
+            </div>
+        </div>
+        <div class="footer">
+            <img src="https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png"style="
+            position: relative;
+            height: 60px;
+            width: 50px;
+            border-radius: 5px;
+            ">
+            <h2>Altin Selimi</h2>
+            <h3>Developer</h3>
+            <a href=""><svg class="feather feather-linkedin sc-dnqmqq jxshSx" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="white" stroke="white" stroke-width="0" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" data-reactid="696">
+                <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+                <rect x="2" y="9" width="4" height="12"></rect>
+                <circle cx="4" cy="4" r="2"></circle>
+            </svg></a>
+        </div>
+    </div>
 </template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import card from '@/components/card.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+      card
+  },
+  data() {
+    return {
+      activeIndex: 0,
+      notes: [{
+        smName: 'Mydevportfol.io',
+        url: 'https://chippd.github.io/deskportfolio/devportfolio.png',
+        description: 'A portfolio site builder for developers',
+        more: 'uses ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '},
+        {smName: 'Jobb.ie',
+        url: 'https://chippd.github.io/deskportfolio/jobbie.png',
+        description: 'A jobs site aggregator/scraper',
+        more: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '},
+        {smName: 'Slack chat bot',
+        url: 'https://chippd.github.io/deskportfolio/slack.png',
+        description: 'For checking domain availability in Slack',
+        more: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '}]
+    }
+  },
+  methods: {
+    noteClass(index) {
+        return `card-${index+1}`;
+    },
+    hapuSusam(index) {
+        this.activeIndex = index;
+        let element = this.$refs.container;
+        let target = document.querySelector(`.card-${index+1}`);
+        setTimeout(() => {
+          target.scrollIntoView({behavior: "smooth",block: "center",inline: "center"});
+        }, 0);
+    },
   }
 }
 </script>
+<style lang="scss">
+body {
+    position: relative;
+    margin: 0;
+    max-width: 100vw;
+    overflow: hidden;
+    height: 100vh;
+    box-sizing: content-box;
+}
+
+.bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 120%;
+    z-index: 0;
+}
+
+.container {
+    position: relative;
+    overflow-x: scroll;
+    display: flex;
+    width: 100vw;
+    //padding-top: 40px;
+}
+
+.container::-webkit-scrollbar {
+    display: none;
+}
+
+.carousel-item-spacer {
+    min-width: 400px;
+    visibility: hidden;
+    height: 300px;
+}
+
+.footer {
+    padding: 40px 0;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 40px;
+
+    h2 {
+        margin: 0;
+        margin-top: 10px;
+        font-size: 18px;
+        color: black;
+        font-weight: 500;
+    }
+
+    h3 {
+        margin-top: 5px;
+        margin-bottom: 10px;
+        font-size: 15px;
+        color: black;
+        font-weight: 400;
+    }
+
+    svg {
+        padding: 2px;
+        border-radius: 3px;
+        border: solid 1px gray;
+        background-color: black;
+    }
+}
+
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    text-align: center;
+    color: #2c3e50;
+    height: 100vh;
+    display: flex;
+    flex-direction:column;
+    justify-content:space-between;
+    position: relative;
+}
+</style>
